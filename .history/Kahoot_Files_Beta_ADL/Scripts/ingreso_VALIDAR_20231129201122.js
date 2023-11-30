c1af3ee156eae83608576789cec3fe3c14e58521
eschala <eduar.chala28@outlook.com>
2023-11-29 20:37:49 -0500

    Organizacion de Carpetas y correciones
    Carpetas ORGANIZADAS

---
+++ .history/Kahoot_Files_Beta_ADL/Scripts/ingreso_VALIDAR_20231129201122.js
@@ -0,0 +1,19 @@
+const redirigirA_Quiz = () => {
+
+    location.href='/';
+  };
+
+function ingresarJuego() {
+  const pinInput = document.getElementById("pinInput").value;
+
+  if (pinInput) {
+    alert("Ingresando al juego con PIN: " + pinInput);
+    
+    setTimeout(redirigirA_Quiz, 3000);
+    // Puedes redirigir a la siguiente página, realizar verificaciones, etc.
+  } else {
+    alert("Por favor, ingresa un PIN válido.");
+  }
+
+
+}
---
+++ Kahoot_Files_Beta_ADL/Scripts/Variables_Kahoot copy.js
@@ -0,0 +1,152 @@
+/* AnswerDefault_Kahoot_SAMIR Pregunta 1 y 2 */
+
+
+  const bodyElement = document.querySelector(".body_Answer");
+
+  const mainContainer = document.querySelector(".mainContainer_Answer");
+
+  const container = document.querySelector(".container_1");
+  const container2 = document.querySelector(".container_2");
+  const container3 = document.querySelector(".container_3");
+
+  const subContainer = document.querySelector(".container_1__sub");
+  const subContainer2_1 = document.querySelector(".container_2__sub_1");
+  const subContainer2_2 = document.querySelector(".container_2__sub_2");
+  const container_3__sub = document.querySelector(".container_3__sub");
+  const subContainer2_1_1 = document.querySelector(".container_2__sub_1__1");
+  const subContainer2_1_2 = document.querySelector(".container_2__sub_1__2");
+
+  const containerButtonSiguiente = document.querySelector(".div_btn_Siguiente");
+
+  const buttonSiguiente = document.querySelector(".btn_Siguiente");
+
+  const tituloSiguiente = document.querySelector(".titulo_Siguiente");
+
+  const timerElement = document.querySelector(".timer");
+  const numeradorRespuestas = document.querySelector(".numAnswer");
+  const textRespuesta = document.querySelector(".Answer");
+
+  const buttonRespuesta1 = document.querySelector(".Answer_1");
+  const buttonRespuesta2 = document.querySelector(".Answer_2");
+  const buttonRespuesta3 = document.querySelector(".Answer_3");
+  const buttonRespuesta4 = document.querySelector(".Answer_4");
+
+  /* AnswerDefault_Kahoot_SAMIR Falso Verdadero 3 y 4 */
+
+  const bodyElement_VF = document.querySelector(".body_Answer_VF");
+
+  const mainContainer_VF = document.querySelector(".mainContainer_Answer_VF");
+
+  const container_VF = document.querySelector(".container_1_VF");
+  const container2_VF = document.querySelector(".container_2_VF");
+  const container3_VF = document.querySelector(".container_3_VF");
+
+  const subContainer_VF = document.querySelector(".container_1__sub_VF");
+  const subContainer2_1_VF = document.querySelector(".container_2__sub_1_VF");
+  const subContainer2_2_VF = document.querySelector(".container_2__sub_2_VF");
+  const container_3__sub_VF = document.querySelector(".container_3__sub_VF");
+  const subContainer2_1_1_VF = document.querySelector(".container_2__sub_1__1_VF");
+  const subContainer2_1_2_VF = document.querySelector(".container_2__sub_1__2_VF");
+
+  const containerButtonSiguiente_VF = document.querySelector(".div_btn_Siguiente_2");
+
+  const buttonSiguiente_VF = document.querySelector(".btn_Siguiente_2");
+
+  const tituloSiguiente_VF = document.querySelector(".titulo_Siguiente_2");
+
+  const timerElement_VF = document.querySelector(".timer_VF");
+  const numeradorRespuestas_VF = document.querySelector(".numAnswer_VF");
+  const textRespuesta_VF = document.querySelector(".Answer_VF");
+
+  const buttonRespuesta1_VF = document.querySelector(".Answer_V");
+  const buttonRespuesta2_VF = document.querySelector(".Answer_F");
+
+
+
+  export {
+
+    bodyElement,
+    mainContainer,
+    container,
+    container2,
+    container3,
+    subContainer,
+    subContainer2_1,
+    subContainer2_2,
+    container_3__sub,
+    subContainer2_1_1,
+    subContainer2_1_2,
+    containerButtonSiguiente,
+    buttonSiguiente,
+    tituloSiguiente,
+    timerElement,
+    numeradorRespuestas,
+    textRespuesta,
+    buttonRespuesta1,
+    buttonRespuesta2,
+    buttonRespuesta3,
+    buttonRespuesta4,
+    bodyElement_VF,
+    mainContainer_VF,
+    container_VF,
+    container2_VF,
+    container3_VF,
+    subContainer_VF,
+    subContainer2_1_VF,
+    subContainer2_2_VF,
+    container_3__sub_VF,
+    subContainer2_1_1_VF,
+    subContainer2_1_2_VF,
+    containerButtonSiguiente_VF,
+    buttonSiguiente_VF,
+    tituloSiguiente_VF,
+    timerElement_VF,
+    numeradorRespuestas_VF,
+    textRespuesta_VF,
+    buttonRespuesta1_VF,
+    buttonRespuesta2_VF
+
+
+  }
+/* 
+  export const bodyElement=;
+  export const mainContainer=;
+  export const container=;
+  export const container2=;
+  export const container3=;
+  export const subContainer=;
+  export const subContainer2_1=;
+  export const subContainer2_2=;
+  export const container_3__sub=;
+  export const subContainer2_1_1=;
+  export const subContainer2_1_2=;
+  export const containerButtonSiguiente=;
+  export const buttonSiguiente=;
+  export const tituloSiguiente=;
+  export const timerElement=;
+  export const numeradorRespuestas=;
+  export const textRespuesta=;
+  export const buttonRespuesta1=;
+  export const buttonRespuesta2=;
+  export const buttonRespuesta3=;
+  export const buttonRespuesta4=;
+  export const bodyElement_VF=;
+  export const mainContainer_VF=;
+  export const container_VF=;
+  export const container2_VF=;
+  export const container3_VF=;
+  export const subContainer_VF=;
+  export const subContainer2_1_VF=;
+  export const subContainer2_2_VF=;
+  export const container_3__sub_VF=;
+  export const subContainer2_1_1_VF=;
+  export const subContainer2_1_2_VF=;
+  export const containerButtonSiguiente_VF=;
+  export const buttonSiguiente_VF=;
+  export const tituloSiguiente_VF=;
+  export const timerElement_VF=;
+  export const numeradorRespuestas_VF=;
+  export const textRespuesta_VF=;
+  export const buttonRespuesta1_VF=;
+  export const buttonRespuesta2_VF=;
+   */
